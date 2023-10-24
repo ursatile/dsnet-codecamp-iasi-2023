9dd055dc9e782c05a042d57aa4f24bffd61013cf
Dylan Beattoie <dylan@dylanbeattie.net>
2023-10-24 14:27:30 +0300

    Standalone pub/sub demo

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -1,12 +1,21 @@
-﻿using EasyNetQ;
+﻿using System.Runtime.CompilerServices;
+using EasyNetQ;
 using Messages;
 
 const string AMQP = "amqps://efwzzuku:7CflikvF4dquXhPcz_lzIcGQJOUJM1Vd@strong-golden-bat.rmq4.cloudamqp.com/efwzzuku";
 var bus = RabbitHutch.CreateBus(AMQP);
-var subscriptionId = "subscriber";
-await bus.PubSub.SubscribeAsync<Greeting>(subscriptionId,
-    Console.WriteLine
+var subscriptionId = "dylan-framework-laptop";
+await bus.PubSub.SubscribeAsync<Greeting>(
+    subscriptionId,
+    Handle
 );
 
 Console.WriteLine("Listening for messages... press Ctrl-C to quit");
 Console.ReadKey(true);
+
+void Handle(Greeting greeting) {
+    if (greeting.Number % 4 == 0) {
+        throw new Exception("Greetings divisible by 4 are not allowed");        
+    }
+    Console.WriteLine(greeting);
+}
